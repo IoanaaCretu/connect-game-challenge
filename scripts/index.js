8f443f6b1f34c9418d0db9683ce100a0674033f6
Ioana <cretuioana.dev@gmail.com>
2023-11-15 19:16:46 +0200

    completed layout for game-board

--- scripts/index.js
+++ scripts/index.js
@@ -9,3 +9,14 @@ const checkRulesButton = document.querySelector(".check-button");
 checkRulesButton.addEventListener("click", () => {
   rulesScreen.classList.add("hidden");
 });
+
+const mainMenu = document.querySelector(".main-menu");
+const gameContainer = document.querySelector(".game");
+const startGameButton = document.querySelector(".play-btn");
+
+startGameButton.addEventListener("click", () => {
+  mainMenu.classList.add("hidden");
+  gameContainer.classList.remove("hidden");
+
+  document.body.style.backgroundColor = "#7945ff";
+});
